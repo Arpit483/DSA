8b6026dbdd13179e7885c4772f34bd261fb3acf6
Arpit <arpitdeosthale12@gmail.com>
2024-08-17 17:11:38 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Practice/arrays/PairsArray.java
    	new file:   bin/arrays/Pair.class
    	modified:   bin/arrays/PairsArray.class

--- Practice/arrays/PairsArray.java
+++ Practice/arrays/PairsArray.java
@@ -1,5 +1,26 @@
 package arrays;
 
+class Pair{
+    public static void p(int arr[]){
+        for (int i = 0 ; i <arr.length ; i++){
+           
+            for (int j = i+1; j <arr.length  ; j++) {
+
+                System.out.print( "( "+  arr[i]+ "  " +arr[j] + " )");
+                
+            }
+        }
+
+    }
+}
+
+
 public class PairsArray {
+    public static void main(String[] args) {
+    Pair p = new Pair();
+    int arr[] = {1,2,3,4,5};
+    p.p(arr);
+        
+    }
     
 }
