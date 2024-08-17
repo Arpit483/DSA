98b796901046dac5cb60bdf6f2a38f1448577af5
Arpit <arpitdeosthale12@gmail.com>
2024-08-17 16:44:38 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Practice/arrays/BinarySearch.java
    	modified:   bin/arrays/BinarySearch.class
    	new file:   bin/arrays/Search.class

--- Practice/arrays/BinarySearch.java
+++ Practice/arrays/BinarySearch.java
@@ -1,8 +1,43 @@
 package arrays;
 
+import java.util.Scanner;
+
+class Search{
+
+    public void sort(int arr[] , int key){
+    
+    int start = 0; int end = arr.length-1;
+    while(start<=end){
+        
+        int mid = (start + end )/2;
+        if(arr[mid] == key){
+            System.out.println("Element is present at index " + (mid+1));
+        }
+        if (arr[mid]< key ) {
+            start = mid+1;
+            
+        } else {
+            end = mid -1;
+        }
+    }
+    
+}
+}
+
+
 public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Scanner sc = new Scanner(System.in);
+       
+        int arrays[] = {2,4,6,8,10,12};
+        for (int i = 0; i < arrays.length; i++) {
+           System.out.print(arrays[i]+" "); 
+        }
+        System.out.println();
+        int key = sc.nextInt();
+        Search se= new Search();
+        se.sort(arrays, key);
+        
     }
     
 }
